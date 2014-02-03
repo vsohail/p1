83729cb741442184f9aabbe7dea371e414d5318d
Sohil <vsohail@gmail.com>
2014-02-03 18:36:58 -0500

    Tested

--- config.mk
+++ config.mk
@@ -30,7 +30,7 @@ TABSTOP = 8
 # the object files which make up your drivers.
 ##################################################
 #
-COMMON_OBJS = console.o handler_install.o timer_handler.o kbd_handler.o timer.o kbd.o game_controller.o
+COMMON_OBJS = console.o handler_install.o timer_handler.o kbd_handler.o timer.o kbd.o
 
 ##################################################
 # Object files from 410kern/ for just the game
@@ -47,7 +47,7 @@ COMMON_OBJS = console.o handler_install.o timer_handler.o kbd_handler.o timer.o 
 # multiple parts.
 ##################################################
 #
-KERN_GAME_OBJS = game.o
+KERN_GAME_OBJS = game.o game_controller.o
 
 ##################################################
 # Object files from 410kern/ for just the tester
